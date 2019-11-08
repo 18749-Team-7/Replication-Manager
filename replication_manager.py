6d539fdbeb7cf4a4c20d5ee564d73e2a470362fe
AshwinRJ <ashwinr001@gmail.com>
2019-11-08 18:20:13 -0500

    Added ReplicationManager class

---
+++ replication_manager.py
@@ -0,0 +1,64 @@
+import json
+import random
+
+
+
+class ReplicationManager:
+    """
+    Alien Tech.
+    """
+
+    def __init__(self, mode='active'):
+        self.mode = mode
+        self.membership = []
+        self.primary = None
+
+    def establish_membership(self, gfd_init_info):
+        """
+        Function to establish initial membership of replicas based on GFD feedback.
+
+        param gfd_init_info: Dict() where keys-IPs of relica servers, values- Init status.
+        """
+
+        for member, status in gfd_init_info.item():
+            if status:
+                self.membership.append(self.map(member))
+
+
+    def modify_membership(self, gfd_info):
+        """
+        Function modifies the membership based on the updates from the GFD.
+
+        param gfd_info: Dict() where keys-IPs of relica servers, values- updated status.
+        """
+        
+        for member, status in gfd_info.item():
+            if status:
+                if member not in self.membership:
+                    self.membership.append(member)
+            else:
+                self.membership.remove(member)
+
+                # Elect a new primary if running on passive mode.
+                if self.mode == 'passive':
+                    if member == self.primary:
+                        self.pick_primary()
+                
+        return 
+
+
+    def pick_primary(self):
+        """
+        Randomly pick a replica as primary from all members.
+
+        """
+        # Change this later!!!
+        self.primary = random.choice(self.membership)
+        return
+
+    
+    def get_replica_ips(self):
+        """
+        Returns the IPs of all the current members.
+        """
+        return self.membership
